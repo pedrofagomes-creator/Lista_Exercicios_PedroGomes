d803687e6ffd81547776a2a998b9b5863b6ba161
PedroGomes_ <pedropppfag@gmail.com>
2026-09-17 19:30:45 -0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -5,30 +5,27 @@
 * Data: 17/09/2026
 */
 
-#include <stdio.h>
-#include <stdlib.h>
 
-int maint(){
-
-    int user, number=rand() % 101;
-
-    printf("Escolha um numero entre 1 e 100");scanf("%d",&number);
-
-    while (user!=number)
-    {
-        if (user<number)
-        {
-            printf("Errado, o numero e maior\n");
-            printf("Escolha um numero entre 1 e 100");scanf("%d",&number);
-        }
-        else if (user>number)
-        {
-            printf("Errado, o numero e mmenor\n");
-            printf("Escolha um numero entre 1 e 100");scanf("%d",&number);
-        }
-    }
-
-    printf("Correto o numero e %d",number);
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+int main() { 
     
-    return 0;
+    int user, number = rand() % 100 + 1; 
+    
+    printf("Escolha um numero entre 1 e 100: "); scanf("%d", &user); 
+    
+    while (user != number) { 
+        if (user < number) { 
+            printf("Errado, o numero e maior\n");
+            printf("Escolha outro numero: "); scanf("%d", &user);
+        } 
+        else { 
+            printf("Errado, o numero e menor\n"); }
+            printf("Escolha outro numero: "); scanf("%d", &user);  
+        }
+
+            printf("Correto! O numero era %d\n", number); 
+        
+    return 0; 
 }
